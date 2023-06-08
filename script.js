951d386cd1c66a494ba491cdaa463880ce9ded58
Ira Bukhaeva <irina.bukhaeva@yandex.ru>
2023-06-08 08:29:07 +0800

    fixed

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-let price1 = document.querySelector(".price1");
-let price2 = document.querySelector(".price2");
-let price3 = document.querySelector(".price3");
-let price4 = document.querySelector(".price4");
+let price1 = document.querySelector(".price1").textContent;
+let price2 = document.querySelector(".price2").textContent;
+let price3 = document.querySelector(".price3").textContent;
+let price4 = document.querySelector(".price4").textContent;
 let sum = document.querySelector(".sum");
 let sumDiscount = document.querySelector(".sumDiscount");
 const button = document.querySelector(".button");
 
-sum.innerHTML =
-  Number(price1) + Number(price2) + Number(price3) + Number(price4);
+sum.innerHTML = +price1 + +price2 + +price3 + +price4;
 
 button.addEventListener("click", function () {
-  sumDiscount.innerHTML = Number(sum) * Number(0.8);
+  sumDiscount.innerHTML = +sum.textContent * 0.8;
 });
-
-console.log(price1);
-console.log(price2);
-console.log(price3);
-console.log(price4);
-console.log(sum);
-console.log(sumDiscount);
